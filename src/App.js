ae3e47632bb93359c402f2b3b3699a4420dc070f
Manuel <msalguero_2@hotmail.com>
2020-10-11 04:35:40 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Landing from './Landing';
+import Projects from './Projects';
+import Portfolio from './Portfolio';
+import Profile from './Profile';
+import CreateProject from './CreateProject';
+import Positions from './Positions';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+            <Route path="/projects">
+              <Projects />
+            </Route>
+            <Route path="/portfolio">
+              <Portfolio />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/create-project">
+              <CreateProject />
+            </Route>
+            <Route path="/positions/:id" >
+              <Positions />
+            </Route>
+            <Route path="/">
+              <Landing />
+            </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
